06f5a6b57e09a2c697d26a6f7f65196df7f341d2
shivahanifi <shiva.hnf@gmail.com>
2024-10-15 09:26:36 +0000

    predict_script updated

--- keras_segmentation/predict_script.py
+++ keras_segmentation/predict_script.py
@@ -3,8 +3,8 @@ from keras_segmentation.models.unet import vgg_unet
 model = vgg_unet(n_classes=51 ,  input_height=416, input_width=608  )
 
 model.train(
-    train_images =  "share/dataset1/images_prepped_train/",
-    train_annotations = "share/dataset1/annotations_prepped_train/",
+    train_images =  "/SCDD-image-segmentation-keras/share/dataset1/images_prepped_train",
+    train_annotations = "/SCDD-image-segmentation-keras/share/dataset1/annotations_prepped_train",
     checkpoints_path = "/tmp/vgg_unet_1" , epochs=5
 )
 
